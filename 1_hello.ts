a3928b050dae76d739592326960c2cceab68e51b
LokeshMurali <lokeshmurrali@gmail.com>
2024-12-09 10:07:21 +0530

    Added hello world in typescript

---
+++ 1_hello.ts
@@ -0,0 +1,8 @@
+console.log("Hello Typescript!");
+
+//.ts --> tsc(TS compiler) --> .js --> Javascript Runtime Env: Browser, nodeJS, App
+
+//Data types
+let age: number = 25;
+let fullName: string = "Lokesh";
+let isActive: boolean = true;
